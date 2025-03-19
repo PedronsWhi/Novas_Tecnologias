efabd6ca83107c2b89a738eb25f17b22efce9f83
PEDRO SÉRGIO DOS SANTOS CAMPOS <pedro.campos@a.ucb.br>
2025-03-19 09:53:13 -0300

    Iniciando diciplina

---
+++ Aula02/main.py
@@ -0,0 +1,42 @@
+# Usando o comando "for"
+"""for i in range(10):
+   print(i)"""
+
+#Usa-se o "c+=1" para que não vire um looper
+"""c = 0
+while c < 10:
+     print(c)
+     c+=1"""
+
+# Usando o termo continue 
+"""for i in range(10):
+    if i == 5:
+        continue
+    print(i)"""
+
+#Usando o termo break
+"""for i in range(10):
+    if i == 5:
+        break
+    print(i)"""
+
+
+#arr = [1,2,3,4,5,6,7,8,9,10]
+# Esse comando percorre toda a lista e imprime os 3 nomes um de casa vez 
+"""arr_names = ["Victor","Zerretos","Fujita"]
+
+for i in arr_names:
+    print(i)"""
+
+# Se x e y forem maior que "zero" o comando printa que ambos são positivos
+"""x = 10
+y = 20
+
+if x > 0 and y > 0:
+    print("Amboas x e y são positivos")
+
+if x > 0 or y > 0:
+    print("Pelo menos um deles é positivo")
+
+if not x > 0:
+    print("x não é maior que 0")"""
